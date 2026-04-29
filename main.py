78259e37e65187d3119654aa1db7e6a2a622fbe4
Yiğit Hasan Demirhan <hasanyigitdemirhan@gmail.com>
2026-04-29 15:55:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,16 @@ line = Line(p1, p2)
 
 print(line)
 print("Length:", line.length())
+
+
+
+from pa_package.point import Point
+from pa_package.line import Line
+
+p1 = Point(0, 0)
+p2 = Point(3, 4)
+
+line = Line(p1, p2)
+
+print(line)
+print("Length:", line.length())
